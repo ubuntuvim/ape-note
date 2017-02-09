8be97043fff47b7ebaf0e81fba6dcf5eeff0a837
ubuntuvim <1527254027@qq.com>
2017-02-10 02:59:59 +0800

    更新最新版本的wildember，修复无法保存model关系问题

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,19 +1,9 @@
-/*
-import FirebaseAdapter from 'emberfire/adapters/firebase';
+//
+// import FirebaseAdapter from 'emberfire/adapters/firebase';
+//
+// export default FirebaseAdapter.extend({
+// });
 
-export default FirebaseAdapter.extend({
-});
-*/
-/*
-import WildemberAdapter from 'wildember/adapters/wildember';
-
-export default WildemberAdapter.extend({
-    wilddogConfig: {
-        syncDomain: "ape-note1.wilddog.com",
-        syncURL: "https://ape-note1.wilddogio.com" //输入节点 URL
-    }
-});
-*/
 import WildemberAdapter from 'wildember/adapters/wildember';
 
 export default WildemberAdapter.extend({
