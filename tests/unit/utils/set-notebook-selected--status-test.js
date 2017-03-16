274e2f90c4f155ccd5ba2076a7d559a031add8fa
ubuntuvim <1527254027@qq.com>
2017-03-17 02:19:28 +0800

    完成计划v1.3.1,增加一份使用手册。

---
+++ tests/unit/utils/set-notebook-selected--status-test.js
@@ -0,0 +1,10 @@
+import setNotebookSelectedStatus from 'ape-note/utils/set-notebook-selected--status';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | set notebook selected  status');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = setNotebookSelectedStatus();
+  assert.ok(result);
+});
